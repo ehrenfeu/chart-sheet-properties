10f1e61d49791308dd627e8f7afbc4cf26c06061
Niko Ehrenfeuchter <segeln@he1ix.org>
2015-02-13 14:41:56 +0100

    More sophisticated parsing and printing.

--- extract_tables_from_xml.py
+++ extract_tables_from_xml.py
@@ -1,21 +1,42 @@
+#!/usr/bin/env python
+
 import xml.etree.ElementTree as etree
+from os.path import join
 
-def print_table(tables, idx):
-    for drow in tables[idx].find('tbody').iter('data_row'):
-    print '********************** data row **********************'
-    for cell in drow.iter('cell'):
-        print cell.text.strip()
-        if cell.attrib['colspan'] != '1':
-            print 'colspan warning! ------------------------------------------------------------------------------------------'
+basedir = '/home/ehrenfeu/usr/packages/OpenSeaMap/S-11_PartB_RegionD_e2.0.5'
+filename = 'output.xml'
 
-tree = etree.parse('output.xml')
-tables = tree.findall('table')
-tables[0].find('title').text
+def print_table(table):
+    print('******** Printing table ********')
+    print('TITLE: %s ' % table.find('title').text)
 
-tables[0].find('header').find('header_line').findall('header_element')[0].text
-for h_element in tables[0].find('header').find('header_line').iter('header_element'):
-    print h_element.text
+    header = ''
+    for h_element in table.find('header').find('header_line').iter('header_element'):
+        header = header + "," + h_element.text.strip()
+    print('HEADER: %s ' % header)
 
-print_table(tables, 0)
-print_table(tables, 1)
-print_table(tables, 2)
+    for drow in table.find('tbody').iter('data_row'):
+        row = ''
+        for cell in drow.iter('cell'):
+            row = row + "," + cell.text.strip()
+            if cell.attrib['colspan'] != '1':
+                print 'colspan warning! ------------------------------------------------------------------------------------------'
+        print(row)
+    print('')
+
+def process_pdf2table_xml():
+    print('Parsing "%s"...' % join(basedir, filename))
+    tree = etree.parse(join(basedir, filename))
+    tables = tree.findall('table')
+
+    # for table in tables:
+    #     print_table(table)
+    print_table(tables[0])
+    print_table(tables[1])
+    print_table(tables[2])
+    print_table(tables[5])
+
+
+if __name__ == "__main__":
+    print('Running XML parser')
+    process_pdf2table_xml()
